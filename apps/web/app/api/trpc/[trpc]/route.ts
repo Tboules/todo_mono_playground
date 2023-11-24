783950c016b6b89666ef704ef82cde0464c9657a
Tboules <tboules@gmail.com>
2023-11-24 13:36:48 -0800

    trpc router init

---
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,29 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+
+import { appRouter } from "@repo/api";
+
+/**
+ * Configure basic CORS headers
+ * You should extend this to match your needs
+ */
+function setCorsHeaders(res: Response) {
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Request-Method", "*");
+  res.headers.set("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+  res.headers.set("Access-Control-Allow-Headers", "*");
+}
+
+const handler = async (req: Request) => {
+  const response = await fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => ({}),
+  });
+
+  setCorsHeaders(response);
+
+  return response;
+};
+
+export { handler as GET, handler as POST };
