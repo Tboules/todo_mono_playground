3adcad08b7ba99e64bf76f6b26ab2d5485accc17
Tboules <tboules@gmail.com>
2023-11-25 11:22:05 -0800

    drizzle migration error fixed

--- packages/db/src/migrate.ts
+++ packages/db/src/migrate.ts
@@ -2,7 +2,7 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import db, { drizzlePGClient } from "./db.js";
 
 await migrate(db, {
-  migrationsFolder: "./drizzle",
+  migrationsFolder: "./drizzle/migrations",
 });
 
 await drizzlePGClient.end();
