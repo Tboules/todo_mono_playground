43b5c7b878efd4293488fa3789dafe046d9d7b08
Tboules <tboules@gmail.com>
2023-11-28 12:00:35 -0800

    fill implementation using all methods except get by id

--- apps/web/app/_components/TodoList.tsx
+++ apps/web/app/_components/TodoList.tsx
@@ -5,6 +5,9 @@ import styles from "../styles.module.css";
 
 export default function TodoList() {
   const todosQuery = trpc.todos.all.useQuery();
+  const todosUpdate = trpc.todos.update.useMutation();
+  const todosDelete = trpc.todos.delete.useMutation();
+  const utils = trpc.useUtils();
 
   if (todosQuery.isLoading) {
     return (
@@ -27,8 +30,41 @@ export default function TodoList() {
       {todosQuery.data.map((todo) => {
         return (
           <div className={styles.todoCard} key={todo.id}>
-            <p>{todo.task}</p>
-            <p>{todo.status}</p>
+            <p className={styles.label}>{todo.task}</p>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              onChange={(e) => {
+                todosUpdate.mutate(
+                  {
+                    ...todo,
+                    status: e.target.checked == true ? "done" : "in-progress",
+                    lastUpdated: new Date(Date.now()),
+                  },
+                  {
+                    onSuccess: () => {
+                      utils.todos.invalidate();
+                    },
+                  },
+                );
+              }}
+              checked={todo.status == "done"}
+              disabled={todosUpdate.isLoading}
+            />
+
+            <button
+              disabled={todosDelete.isLoading}
+              onClick={() => {
+                todosDelete.mutate(
+                  { id: todo.id },
+                  {
+                    onSuccess: () => utils.todos.invalidate(),
+                  },
+                );
+              }}
+            >
+              D
+            </button>
           </div>
         );
       })}
