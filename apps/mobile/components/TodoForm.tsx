d6217efff0dcf9f79a461d6742e8cbfe757273b4
Tboules <tboules@gmail.com>
2023-12-04 11:15:08 -0800

    prettier

--- apps/mobile/components/TodoForm.tsx
+++ apps/mobile/components/TodoForm.tsx
@@ -1,24 +1,20 @@
-import {
-  TextInput,
-} from "react-native";
+import { TextInput } from "react-native";
 import { trpc } from "../_trpc/client";
 import { useState } from "react";
 
 export default function TodoForm() {
   const todoMutation = trpc.todos.create.useMutation();
   const utils = trpc.useUtils();
-  const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>("");
 
-  function handleSubmit(
-  ) {
-
+  function handleSubmit() {
     if (value) {
       todoMutation.mutate(
         { task: value },
         {
           onSuccess: () => {
             utils.todos.all.invalidate();
-            setValue('')
+            setValue("");
           },
         },
       );
